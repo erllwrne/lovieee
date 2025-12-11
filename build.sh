e05240b640846ceb052769f171d0ea899e7b0830
erllwrne <sigue.418262@stamaria.sti.edu.ph>
2025-12-11 15:25:22 +0800

    Fix Pillow install for Render

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on any error
 set -o errexit
 
+# Upgrade pip first (helps with dependencies like Pillow)
+python -m pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
